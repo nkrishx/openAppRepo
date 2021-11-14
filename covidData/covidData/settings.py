51ab1970c1ae81be77549ea9990d38f75bf21ed9
nkrishx <naveen.gowda.rm@gmail.com>
2021-11-14 04:02:24 +0000

    remove secret key

--- covidData/covidData/settings.py
+++ covidData/covidData/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-avy=$06uhy!(rf01bas0r9banpy8(5vx5lspt6q)f_sari26b*'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -150,3 +151,9 @@ CORS_ORIGIN_WHITELIST = (
 REST_FRAMEWORK = {
    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
    }
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass

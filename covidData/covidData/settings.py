ab2f149e8af35959898cbdbe8327a8927453274f
nkrishx <naveen.gowda.rm@gmail.com>
2021-11-15 03:30:35 +0000

    settings config

--- covidData/covidData/settings.py
+++ covidData/covidData/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-avy=$06uhy!(rf01bas0r9banpy8(5vx5lspt6q)f_sari26b*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,16 +87,12 @@ WSGI_APPLICATION = 'covidData.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'coviddatadb',
-        'USER': 'postgres',
-        'PASSWORD': 'testpassword',
-        'HOST': 'localhost'
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST')
     }
 
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
 }
 
 
